de2aaa06eaeebdd110f3ac5a4f35883d4c386f36
Laura McFarlane <lauramcf@uw.edu>
2013-11-25 07:43:24 -0800

    hw4

--- js/menu-main.js
+++ js/menu-main.js
@@ -10,9 +10,12 @@ function render(entries) {
     var instance;
     var menu = $('.content');
     $.each(entries, function(index, value){
-            alert(value);
         instance = $('.pizzaTemplate').clone();
         instance.find('.name').html(value.name);
+        instance.find('.description').html(value.description);
+        instance.find('.prices').html(value.prices);
+        instance.find('.price').html(value.price);
+
         //instance.find('.description').html(this.description);
         //instance.find('.prices').html(this.prices);
         //instance.find('.price').html(this.price);
