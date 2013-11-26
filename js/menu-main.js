c561de7a2dc301cbbcbc0d656deb66d255bc4c4c
Laura McFarlane <lauramcf@uw.edu>
2013-11-25 21:52:25 -0800

    hw

--- js/menu-main.js
+++ js/menu-main.js
@@ -1,25 +1,68 @@
 var menu = com.dawgpizza.menu;
 
 $(function(){
-    render(menu.pizzas);
-    render(menu.drinks);
-    render(menu.desserts);
+    renderPizza(menu.pizzas);
+    renderDrinks(menu.drinks);
+    renderDesserts(menu.desserts);
 });
 
-function render(entries) {
+function renderPizza(entries) {
     var instance;
     var menu = $('.content');
-    $.each(entries, function(index, value){
-        instance = $('.pizzaTemplate').clone();
-        instance.find('.name').html(value.name);
-        instance.find('.description').html(value.description);
-        instance.find('.prices').html(value.prices);
-        instance.find('.price').html(value.price);
+
+    $.each(entries, function(index, pizza){
+        instance = $('.pizzaTemplate').clone(); 
+        
+        if(pizza.vegetarian) {
+            container = $('.vegetarian');
+        }
+        else {
+            container = $('.meat');
+        }
+        instance.find('.name').html(pizza.name);
+        instance.find('.description').html(pizza.description);
+        instance.find('.small-pizza').html("$" + pizza.prices[0]);
+        instance.find('.medium-pizza').html("/ $" + pizza.prices[1]);
+        instance.find('.large-pizza').html("/ $" + pizza.prices[2]);
 
         //instance.find('.description').html(this.description);
         //instance.find('.prices').html(this.prices);
         //instance.find('.price').html(this.price);
         instance.removeClass('pizzaTemplate');
         menu.append(instance);
-    });
+    });   
+}
+
+function renderDrinks(entries) {
+    var instance;
+    var menu = $('.content');
+
+    $.each(entries, function(index, value){
+        instance = $('.drinkTemplate').clone(); 
+        instance.find('.name').html(value.name);
+        instance.find('.price').html("$" + value.price);
+
+        //instance.find('.description').html(this.description);
+        //instance.find('.prices').html(this.prices);
+        //instance.find('.price').html(this.price);
+        instance.removeClass('drinkTemplate');
+        menu.append(instance);
+    });   
+}
+
+function renderDesserts(entries) {
+    var instance;
+    var menu = $('.content');
+
+    $.each(entries, function(index, value){
+        instance = $('.dessertTemplate').clone(); 
+        instance.find('.name').html(value.name);
+        instance.find('.price').html("$" + value.price);
+
+        //instance.find('.description').html(this.description);
+        //instance.find('.prices').html(this.prices);
+        //instance.find('.price').html(this.price);
+        instance.removeClass('dessertTemplate');
+        menu.append(instance);
+    });   
 }
